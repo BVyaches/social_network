3542cf853edbc5a9d9d6ea54a1fa23e1955b2140
Vyacheslav <slavaboll@gmail.com>
2021-03-04 20:44:49 +0500

    ver 1.0 new model

---
+++ social_network/posts/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'text', 'pub_date', 'author')
+    search_fields = ('text',)
+    list_filter = ('pub_date',)
+    empty_value_display = '-pusto-'
+
+
+admin.site.register(Post, PostAdmin)
---
+++ social_network/posts/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class Post(models.Model):
+    text = models.TextField()
+    pub_date = models.DateTimeField('date published', auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    group = models.ForeignKey('Group', on_delete=models.CASCADE, related_name='group')
+
+
+class Group(models.Model):
+    title = models.CharField(max_length=15)
+    slug = models.SlugField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
