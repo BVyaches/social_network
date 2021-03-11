8ce69b93ef462458bd071b504e835aeadada6e08
Vyacheslav <slavaboll@gmail.com>
2021-03-11 20:09:13 +0500

    ver 1.2 Working groups

--- social_network/posts/models.py
+++ social_network/posts/models.py
@@ -19,3 +19,6 @@ class Post(models.Model):
     pub_date = models.DateTimeField('date published', auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
     group = models.ForeignKey(Group, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.text[:15]
